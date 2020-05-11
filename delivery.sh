3da1c7b523ad011482c4c72219e0161ca5027728
Yogendrasingh-Rathore <53259324+Yogendrasingh-Rathore@users.noreply.github.com>
2020-05-11 17:55:51 +0530

    Update delivery.sh

--- delivery.sh
+++ delivery.sh
@@ -25,4 +25,4 @@ set +x
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-java -jar target/cal-1.0-SNAPSHOT.jar -Darguments="${First_Number} ${Second_Number} ${Operation}"
+java -jar target/cal-1.0-SNAPSHOT.jar -Darguments="'${First_Number}' '${Second_Number}' '${Operation}'"
