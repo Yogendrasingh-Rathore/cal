184ae2d7f383c54520709ba0558c843f5ed556fe
Yogendrasingh-Rathore <53259324+Yogendrasingh-Rathore@users.noreply.github.com>
2020-05-12 10:01:12 +0530

    Added try catch

--- src/main/java/com/cuelogic/demo/Calculator.java
+++ src/main/java/com/cuelogic/demo/Calculator.java
@@ -1,26 +1,31 @@
     package com.cuelogic.demo;
 
     import java.util.*;
+    import java.io.*;
 
     public class Calculator {
-
     
     public Calculator(){}
             
 	public static void main(String[] args) {
-		java.io.InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("my.properties");
- 		java.util.Properties properties = new Properties();
- 		properties.load(inputStream);
- 		double first = Double.parseDouble(properties.getProperty("First_Number"));
-		double second = Double.parseDouble(properties.getProperty("Second_Number"));
-		String operator_input = properties.getProperty("Operation");
-		char operator = operator_input.charAt(0);
-		
-		//double first = Double.parseDouble(args[0]);
-        	//double second = Double.parseDouble(args[1]);
-        	//char operator = args[2].charAt(0);
-        	
-		System.out.println(new Calculator().cal(operator,first,second));
+		try{
+			java.io.InputStream inputStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("my.properties");
+			java.util.Properties properties = new Properties();
+			properties.load(inputStream);
+			double first = Double.parseDouble(properties.getProperty("First_Number"));
+			double second = Double.parseDouble(properties.getProperty("Second_Number"));
+			String operator_input = properties.getProperty("Operation");
+			char operator = operator_input.charAt(0);
+
+			//double first = Double.parseDouble(args[0]);
+			//double second = Double.parseDouble(args[1]);
+			//char operator = args[2].charAt(0);
+
+			System.out.println(new Calculator().cal(operator,first,second));
+		}
+		catch(IOException e) {
+  			e.printStackTrace();
+		}
 	}
 	
 	private static double cal(char operator, double first, double second)
